69b909209663aa23bf0da0da16e56b2d530c030a
Gabriel Silvio da Silva Santos <silvio.apad@gmail.com>
2025-10-28 19:15:50 -0400

    Adiciona componentes de layout e pomodoro, incluindo navegação e lógica de temporizador; remove ícones desnecessários e adiciona novas imagens.

--- app/index.tsx
+++ app/index.tsx
@@ -1,130 +1,13 @@
-import { useRef, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { ActionButton } from "./components/ActionButton";
-import { FokusButton } from "./components/FokusButton";
-import { Timer } from "./components/Timer";
-import longImg from "./long.png";
-import pomodoroImg from "./pomodoro.png";
-import shortImg from "./short.png";
-
-const pomodoro = [
-  { initialValue: 25, img: pomodoroImg, text: "Foco" },
-  { initialValue: 5, img: shortImg, text: "Pausa curta" },
-  { initialValue: 15, img: longImg, text: "pausa longa" },
-];
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  const [timerType, setTimerType] = useState(pomodoro[0]);
-  const [seconds, setSeconds] = useState(pomodoro[0].initialValue * 60); // Converte minutos para segundos
-  const [timerRunning, setTimerRunning] = useState(false);
-  const [activeButton, setActiveButton] = useState(0);
-
-  const timerRef = useRef<null | number>(null);
-
-  const clear = () => {
-    if (timerRef.current != null) {
-      clearInterval(timerRef.current);
-      timerRef.current = null;
-      setTimerRunning(false);
-    }
-  };
-
-  const toggleTimerType = (newTimerType: (typeof pomodoro)[0]) => {
-    clear();
-    setTimerType(newTimerType);
-    setSeconds(newTimerType.initialValue * 60); // Converte minutos para segundos
-  };
-
-  function definirBotaoAtivo(index: number) {
-    console.log(`Botão ${index} pressionado`);
-    setActiveButton(index);
-    toggleTimerType(pomodoro[index]);
-  }
-
-  function toggleTime() {
-    if (timerRef.current) {
-      clear();
-      return;
-    }
-
-    setTimerRunning(true);
-
-    const id = setInterval(() => {
-      setSeconds((oldState) => {
-        if (oldState === 0) {
-          clear();
-          return timerType.initialValue * 60; // Reinicia o timer
-        }
-        return oldState - 1;
-      });
-    }, 1000);
-
-    timerRef.current = id;
-  }
-
   return (
-    <View style={styles.container}>
-      <Image source={timerType.img} />
-      <View style={styles.actions}>
-        <View style={styles.context}>
-          {pomodoro.map((button, index) => (
-            <ActionButton
-              key={index}
-              onPress={definirBotaoAtivo}
-              text={button.text}
-              index={index}
-              active={activeButton === index}
-            />
-          ))}
-        </View>
-        <Timer totalSeconds={seconds} />
-        <FokusButton
-          onPress={toggleTime}
-          text={timerRunning ? "Pausar" : "Começar"}
-        />
-      </View>
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          Projeto fictício e sem fins comerciais.
-        </Text>
-        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
-      </View>
+    <View>
+      <Link href={"/pomodoro" as any}>
+        <Text>Go to Pomodoro</Text>
+      </Link>
+      <Text>Home Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#021123",
-    gap: 80,
-  },
-  text: {
-    color: "#FFF",
-  },
-  actions: {
-    paddingHorizontal: 24,
-    paddingVertical: 24,
-    backgroundColor: "#14448080",
-    width: "80%",
-    borderRadius: 32,
-    borderWidth: 2,
-    borderColor: "#144480",
-    gap: 32,
-  },
-  footer: {
-    width: "80%",
-  },
-  footerText: {
-    textAlign: "center",
-    color: "#98A0A8",
-    fontSize: 12.5,
-  },
-  context: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-});
---
+++ app/pomodoro.tsx
@@ -0,0 +1,130 @@
+import { useRef, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import longImg from "../assets/images/long.png";
+import pomodoroImg from "../assets/images/pomodoro.png";
+import shortImg from "../assets/images/short.png";
+import { ActionButton } from "./components/ActionButton";
+import { FokusButton } from "./components/FokusButton";
+import { Timer } from "./components/Timer";
+
+const pomodoro = [
+  { initialValue: 25, img: pomodoroImg, text: "Foco" },
+  { initialValue: 5, img: shortImg, text: "Pausa curta" },
+  { initialValue: 15, img: longImg, text: "pausa longa" },
+];
+
+export default function Pomodoro() {
+  const [timerType, setTimerType] = useState(pomodoro[0]);
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue * 60); // Converte minutos para segundos
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [activeButton, setActiveButton] = useState(0);
+
+  const timerRef = useRef<null | number>(null);
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+      setTimerRunning(false);
+    }
+  };
+
+  const toggleTimerType = (newTimerType: (typeof pomodoro)[0]) => {
+    clear();
+    setTimerType(newTimerType);
+    setSeconds(newTimerType.initialValue * 60); // Converte minutos para segundos
+  };
+
+  function definirBotaoAtivo(index: number) {
+    console.log(`Botão ${index} pressionado`);
+    setActiveButton(index);
+    toggleTimerType(pomodoro[index]);
+  }
+
+  function toggleTime() {
+    if (timerRef.current) {
+      clear();
+      return;
+    }
+
+    setTimerRunning(true);
+
+    const id = setInterval(() => {
+      setSeconds((oldState) => {
+        if (oldState === 0) {
+          clear();
+          return timerType.initialValue * 60; // Reinicia o timer
+        }
+        return oldState - 1;
+      });
+    }, 1000);
+
+    timerRef.current = id;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={timerType.img} />
+      <View style={styles.actions}>
+        <View style={styles.context}>
+          {pomodoro.map((button, index) => (
+            <ActionButton
+              key={index}
+              onPress={definirBotaoAtivo}
+              text={button.text}
+              index={index}
+              active={activeButton === index}
+            />
+          ))}
+        </View>
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          onPress={toggleTime}
+          text={timerRunning ? "Pausar" : "Começar"}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 80,
+  },
+  text: {
+    color: "#FFF",
+  },
+  actions: {
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.5,
+  },
+  context: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
